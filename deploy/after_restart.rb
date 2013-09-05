15ed8e4fdb4c076b14dbe6d282be01acb715e4ee
Jim Lindley <mail@jim.io>
2013-09-05 13:13:54 -0700

    generate 10mb of garbage output on deploy

---
+++ deploy/after_restart.rb
@@ -0,0 +1,12 @@
+byte_count = 0
+target_byte_count = 10_485_760 # ten megabytes
+
+lines = []
+line_count = 0
+
+while byte_count < target_byte_count do
+  lines << line = "#{line_count} " + ("x" * 50) + "\n"
+  byte_count += line.bytesize
+  line_count += 1
+  puts line
+end
