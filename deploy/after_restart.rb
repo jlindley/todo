850c4c8046b7be946bd7174f74751e7ef8ca9f19
Jim Lindley <mail@jim.io>
2013-09-05 13:49:25 -0700

    fix for warning

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -4,7 +4,7 @@ target_byte_count = 10_485_760 # ten megabytes
 line_count = 0
 
 while byte_count < target_byte_count do
-  lines << line = "#{line_count} " + ("x" * 50) + "\n"
+  line = "#{line_count} " + ("x" * 50) + "\n"
   byte_count += line.bytesize
   line_count += 1
   warning line
